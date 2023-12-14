9119ab48f1a8a36110e1c559550e4e805599e422
Kamauleonard <leonardmwangi2012@gmail.com>
2023-12-14 16:27:12 +0300

    Completed Assignment

---
+++ lib/anagram.py
@@ -0,0 +1,11 @@
+
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list):
+        return [w for w in word_list if self.is_anagram(w)]
+
+    def is_anagram(self, other_word):
+        return sorted(self.word.lower()) == sorted(other_word.lower())
